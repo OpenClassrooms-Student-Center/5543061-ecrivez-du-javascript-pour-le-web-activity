40b5692deccbac22e7d3aae8107a053097625b31
tln-lyc-rouv-bts-sn <franck.rivier@ac-nice.fr>
2023-04-15 15:17:32 +0200

    Another change

--- launch_git.bat
+++ launch_git.bat
@@ -1,5 +1,8 @@
 @echo off
 rem @Author Franck
 rem This script allows to add the path pointing to git
+rem You can then type > git config user.name "your-email-adrees"
+rem and > git config user.name "your-git-name-account"
+rem > git config --list
 set "PATH=D:\PortableGit\bin;%PATH%"
 cmd git.exe
