83ba7153c0596d9425659293d875086558bbe9ae
tln-lyc-rouv-bts-sn <franck.rivier@ac-nice.fr>
2023-04-15 16:49:54 +0200

    Another change just to be sure

--- launch_git.bat
+++ launch_git.bat
@@ -4,5 +4,6 @@ rem This script allows to add the path pointing to git
 rem You can now type > git config --global user.name "your-name"
 rem and > git config --global user.email "your-email"
 rem to verify the config > git config --list
+rem to show the config path > git config --list --show-origin
 set "PATH=D:\PortableGit\bin\;%PATH%"
 cmd git.exe
